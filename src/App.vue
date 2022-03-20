90028a08c598ad5271dc7d94a8342ef54fe2d514
Adrian Burlacu <adrian.burlacu@live.com>
2022-03-20 14:49:24 +0200

    Add side nav with expansion panels.

--- src/App.vue
+++ src/App.vue
@@ -4,38 +4,79 @@
       app
       color="primary"
       dark
+      dense
+      elevation="4"
+      z-index="1089"
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group1"
+        >
+          <v-list-item>
+            <v-spacer></v-spacer>
+          </v-list-item>
+
+          
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+          
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header>
+                Category 1
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-list-item>
+                  <v-list-item-title>Bar</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-title>Fizz</v-list-item-title>
+                </v-list-item>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-list-item-group>
+
+        <v-list-item-group
+          v-model="group2"
+        >          
+          <v-list-item>
+            <v-list-item-title>Buzz 2</v-list-item-title>
+          </v-list-item>
+          
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header>
+                Category 2
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-list-item>
+                  <v-list-item-title>Bar 2</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-title>Fizz 2</v-list-item-title>
+                </v-list-item>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <HelloWorld/>
@@ -54,7 +95,19 @@ export default {
   },
 
   data: () => ({
-    //
+    drawer: false,
+    group1: null,
+    group2: null,
   }),
+
+  watch: {
+    group1 () {
+      this.drawer = false
+    },
+    group2 () {
+      this.drawer = false
+    },
+  },
+
 };
 </script>
