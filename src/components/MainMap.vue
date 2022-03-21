06848872f80cef8154d76a884938018c72b9df1f
Adrian Burlacu <adrian.burlacu@live.com>
2022-03-21 23:31:18 +0200

    Use roads from the Overpass API.

--- src/components/MainMap.vue
+++ src/components/MainMap.vue
@@ -25,14 +25,18 @@
 <script>
   import 'ol/ol.css';
   import Map from 'ol/Map';
-  import {OSM, Vector as VectorSource} from 'ol/source';
-  import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+  import {Vector as VectorSource} from 'ol/source';
+  import {Vector as VectorLayer} from 'ol/layer';
   import View from 'ol/View';
+
+  import OSMXML from 'ol/format/OSMXML';
+  import {bbox as bboxStrategy} from 'ol/loadingstrategy';
+  import {transformExtent} from 'ol/proj';
 
   import Geolocation from 'ol/Geolocation';
   import Point from 'ol/geom/Point';
   import Feature from 'ol/Feature';
-  import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+  import {Circle as CircleStyle, Fill, Stroke, Style, Text} from 'ol/style';
 
   export default {
     name: 'MainMap',
@@ -44,16 +48,98 @@
     }),
 
     mounted: async function () {
-      let map = new Map({
-        layers: [
-          new TileLayer({
-            source: new OSM(),
+      
+      const styles = {
+        'highway': {
+          'service': new Style({
+            stroke: new Stroke({
+              color: 'blue',
+              width: 2,
+            }),
+            text: new Text({
+              font: 'bold 16px "Open Sans", "Arial Unicode MS", "sans-serif"',
+              placement: 'line',
+              textBaseline: 'ideographic',
+              fill: new Fill({
+                color: 'blue',
+              }),
+            }),
           }),
-        ],
+          '.*': new Style({
+            stroke: new Stroke({
+              color: 'red',
+              width: 3,
+            }),
+            text: new Text({
+              font: 'bold 16px "Open Sans", "Arial Unicode MS", "sans-serif"',
+              placement: 'line',
+              textBaseline: 'ideographic',
+              fill: new Fill({
+                color: 'red',
+              }),
+            }),
+          }),
+        },
+      };
+
+      const vectorSource = new VectorSource({
+        format: new OSMXML(),
+        loader: function (extent, resolution, projection, success, failure) {
+          const epsg4326Extent = transformExtent(extent, projection, 'EPSG:4326');
+          const client = new XMLHttpRequest();
+          client.open('POST', 'https://overpass-api.de/api/interpreter');
+          client.addEventListener('load', function () {
+            
+            const features = new OSMXML().readFeatures(client.responseText, {
+              featureProjection: map.getView().getProjection(),
+            });
+            vectorSource.addFeatures(features);
+            success(features);
+          });
+          client.addEventListener('error', failure);
+          const query =
+            '(way["highway"](' +
+            epsg4326Extent[1] +
+            ',' +
+            Math.max(epsg4326Extent[0], -180) +
+            ',' +
+            epsg4326Extent[3] +
+            ',' +
+            Math.min(epsg4326Extent[2], 180) +
+            '););out body;>;out skel qt;';
+          
+          client.send(query);
+        },
+        strategy: bboxStrategy,
+      });
+
+      const vector = new VectorLayer({
+        declutter: true,
+        source: vectorSource,
+        style: function (feature) {
+          for (const key in styles) {
+            const value = feature.get(key);
+            if (value !== undefined) {
+              for (const regexp in styles[key]) {
+                if (new RegExp(regexp).test(value)) {
+                  let style = styles[key][regexp];
+                  style.getText().setText(feature.get('name')); 
+                  return style;
+                }
+              }
+            }
+          }
+          return null;
+        },
+      });
+
+      let map = new Map({
+        layers: [vector],
         target: 'map',
         view: new View({
           center: [0, 0],
           zoom: 17,
+          minZoom: 15,
         }),
       });
       this.map = map;
