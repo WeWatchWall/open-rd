caab505389c71bd457e20c7a84349a355f9591c5
Adrian Burlacu <adrian.burlacu@live.com>
2022-03-28 14:10:44 +0300

    Add charts and fourier for audio.

---
+++ src/components/BarChart.vue
@@ -0,0 +1,22 @@
+<script>
+import Chart from 'chart.js/auto';
+import { Bar } from 'vue-chartjs/legacy';
+
+export default {
+  extends: Bar,
+  props: ["chartData", "options"],
+  data: () => ({
+  }),
+  mounted () {
+    console.log(Chart.name);
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.chartData, this.options)
+  },
+  watch: {
+    chartData () {
+      this.$data._chart.update()
+    }
+  }
+}
+</script>
--- src/components/TripHistory.vue
+++ src/components/TripHistory.vue
@@ -2,8 +2,9 @@
   <v-container>
     <v-card
       elevation="2"
+      width="100vw"
     >
-      Test
+      <bar-chart v-if="loaded" :chart-data="chartData" :options="chartOptions"></bar-chart>
     </v-card>
   </v-container>
 </template>
@@ -14,14 +15,56 @@
   import { AudioDataProvider } from "../audio/AudioDataProvider";
   import { Radix2FFT } from "../audio/Radix2FFT";
 
+  import BarChart from "./BarChart.vue";
+  import { ChartOptions } from "chart.js";
+
   @Component({
     name: 'TripHistory',
 
+    components: {
+      BarChart
+    },
+
     data: () => ({
+      loaded: false,
+      chartData: null,
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: true,
+        aspectRatio: 1,
+        animation: {
+          duration: 1
+        },
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            enabled: false
+          }
+        },
+        scales: {
+          x: {
+            ticks: {
+              // For a category axis, the val is the index so the lookup via getLabelForValue is needed
+              callback: function(val, index) {
+                // Hide every 2nd tick label
+                return index % 10 === 0 ? this.getLabelForValue(Number.parseFloat(val.toString())) : '';
+              },
+            }
+          },
+          y: {
+            max: 50
+          }
+        }
+      } as ChartOptions
     }),
 
+    // graphs
+    // worker 
+    //   async interface
+    //   comm interface
     mounted: async function () {
-      // Based on: https://demo.scichart.com/javascript-audio-analyzer-fft-example
       const dataProvider = new AudioDataProvider();
       dataProvider.initAudio();
 
@@ -31,9 +74,11 @@
       const fft = new Radix2FFT(bufferSize);
 
       const hzPerDataPoint = sampleRate / bufferSize;
-      const fftSize = fft.fftSize;
+      // const fftSize = fft.fftSize;
 
-      setTimeout(() => {
+      const labels = Array(100).fill(0).map((_, idx) => (idx + 1) * (Math.round(hzPerDataPoint * 100)));
+
+      setInterval(() => {
         if (dataProvider.initialized === false) {
             return;
         }
@@ -43,10 +88,43 @@
         // Perfrom FFT
         const fftData = fft.run(audioData!.yData);
 
+        const percentFftData = [];
+        let binCount = 1;
+        let binAvg = 0;
+        let binIndex = 0;
+        
+        for (let index = 0; index < fftData.length; index++) {
+          const element = fftData[index];
+          
+          if (index > 0 && index % 10 === 0) {
+            percentFftData.push(binAvg);
+            binCount = 1;
+            binAvg = 0;
+            binIndex++;
+          }
+
+          if (binIndex > 99) { break; }
+
+          binAvg = (binAvg * binCount + Math.abs(element)) / (binCount + 1);
+          binCount++;
+        }
+
         // X axis.
-        console.log(`hzPerDataPoint: ${hzPerDataPoint}, fftSize: ${fftSize}`);
-        console.log(JSON.stringify(fftData)); // Fourier transform. 
-      }, 2000);
+        // console.log(`hzPerDataPoint: ${hzPerDataPoint}, bufferSize: ${bufferSize}, fftSize: ${fftSize}`);
+        // console.log(JSON.stringify(fftData)); // Fourier transform.
+
+        this.$data.chartData = {
+          labels: labels,
+          datasets: [
+            {
+              backgroundColor: "blue",
+              data: percentFftData
+            }
+          ]
+        };
+
+        this.$data.loaded = true;
+      }, 50);
     },
 
     methods: {
